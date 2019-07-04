a1289327d02156a564a4aacfa4333391fb7180e0
rclark848 <rclark848@gmail.com>
2019-07-04 03:36:22 -0400

    Format changes

--- Pull_BFs.R
+++ Pull_BFs.R
@@ -86,3 +86,5 @@ all <- merge(locnames, BFs_ALL, by = "snp_id")
 #write out
 write.table(can_loci, file = "aux1_all_candidate_loci.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
 write.table(all, file = "aux1_all_BFs.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
+
+
