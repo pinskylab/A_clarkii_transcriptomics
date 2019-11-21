ca5207e5c329cb9f7f09a73413d086afea923ab4
rclark848 <rclark848@gmail.com>
2019-11-21 12:03:11 -0500

    Script to run SNP2GO on Amphiprion for SNP enrichment test

---
+++ gene_enrichment.R
@@ -0,0 +1,61 @@
+################################################## Script for Gene Enrichment  ######################################################
+
+#Created for transcriptome project
+#Uses snp2go package among others (snp2go pulled from snp2go.R function on GitHub -> davidszkiba/snp2go)
+#Run on Amphiprion (correct version of GenomicRanges, etc.)
+
+#################################################################################################################################################
+
+######## Set-up ########
+
+#set working directory
+setwd("C:/Users/Rene/Dropbox/Pinsky_Lab/Transcriptome_Proj/R_scripts/A_clarkii_transcriptomics/")
+getwd()
+
+remove(list = ls())
+
+#load libraries
+library(hash)
+library(goProfiles)
+library(GenomicRanges)
+
+#load functions
+source("snp2go.R") #pulled from davidszkiba/snp2go/snp2go.R on GitHub (as all other functions are)
+source("ProcessGFF.R")
+source("ProcessMartExport.R")
+source("GetProgressBarIncrementer.R")
+source("ComputeLevels.R")
+source("RemoveFromUpperLevels.R")
+source("ComputeSNPsByLevel.R")
+source("GetAllRegionsAndSNPs.R")
+source("GetCandidateTerms.R")
+source("GetStatistic.R")
+source("FindLevel.R")
+source("SamplingTest.R")
+source("AddOffspringInformation.R")
+
+#read in data
+cand_snps <- read.delim("../../VCFs_and_PLINK/output.hicov2.snps.only.outliersonly.strict.vcf", header = FALSE, comment.char = "#") #reading in VCF file with cand SNPs only, ignoring header comments
+noncand_snps <- read.delim("../../VCFs_and_PLINK/output.hicov2.snps.only.mac2.nooutliers.strict.vcf", header = FALSE, comment.char = "#") #reading in VCF file with noncand SNPs only, ignoring header comments
+#noncand_snps <- read.delim("../Desktop/Transcriptome_Proj/SNP_VCFs/output.hicov2.snps.only.nooutliers.strict.vcf", header = FALSE, comment.char = "#") #reading in VCF file with noncand SNPs only, ignoring header comments
+
+################################################################################################################################################
+
+######## Create GenomicRanges object from VCFs ########
+cand_snps[, 2] <- as.numeric(cand_snps[, 2]) #making sure SNP bp column is numeric
+cand_snps_GR <- GRanges(seqnames = cand_snps[, 1], ranges = IRanges(cand_snps[, 2], cand_snps[, 2])) #converting cand_snps to GenomicRanges object
+
+noncand_snps[, 2] <- as.numeric(noncand_snps[, 2])
+noncand_snps_GR <- GRanges(seqnames = noncand_snps[, 1], ranges = IRanges(noncand_snps[, 2], noncand_snps[, 2]))
+
+######## Gene enrichment analysis ########
+
+#run on Amphiprion bc have older version of bioconductor that is compatable with lapply()
+snp2go_analysis <- snp2go(gff = "snp2go_annotation_MFBP.gff", candidateSNPs = cand_snps_GR, noncandidateSNPs = noncand_snps_GR)
+
+gff.significant.terms <- snp2go_analysis$enriched$GO
+gff.significant.regions <- snp2go_analysis$regions
+enriched <- snp2go_analysis$enriched
+
+#write out
+write.table(file = "../../Annotation_Results/snp2go_enriched_gff.tsv", enriched, sep = "\t", row.names = FALSE)
