afdf667493832d89f7617be527e00b17812a80a7
rclark848 <rclark848@gmail.com>
2019-12-09 13:56:39 -0500

    Edits for paper figures

--- BF Correlation Plots.R
+++ BF Correlation Plots.R
@@ -19,7 +19,8 @@ library(tidyverse)
 library(gridExtra)
 
 #read in data
-BFs <- read_csv("BF_total.csv", col_names = TRUE)
+BFs <- read_csv("BF_total_mac2.csv", col_names = TRUE)
+#BFs <- read_csv("BF_total_allsnps.csv", col_names = TRUE)
 
 ################################################################################################################################################
 
@@ -35,12 +36,14 @@ SSTmean_SSTmin_plot <- ggplot(data = BFs, aes(x = SST_Mean, y = SST_Min)) + geom
   geom_hline(yintercept = 15, size = 2, linetype = "dashed", color = "#666666") + 
   geom_vline(xintercept = 15, size = 2, linetype = "dashed", color = "#666666") + 
   geom_abline(intercept = 0, slope = 1, size = 2, color = "#999999") + 
-  annotate("text", x = 3, y = 20, label = "r = 0.873", size = 10)
+  annotate("text", x = 7, y = 20, label = "r = 0.926", size = 10) + 
+  annotate("text", x = 3, y = 25, label = "A", size = 15)
 SSTmean_SSTmin_annotated_plot <- SSTmean_SSTmin_plot + ggtitle("SST Mean BF v. SST Min BF") + labs(x = "SST Mean BF", y = "SST Min BF") + 
   theme_bw() + theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
-                     axis.line = element_line(size = 1), plot.title = element_text(size = 14, face = "bold"), 
-                     axis.ticks = element_line(color = "black", size = 1), axis.text = element_text(size = 30, color = "black"), 
-                     axis.title = element_text(size = 18, face = "bold")) + scale_y_continuous(limits = c(0, 25)) + scale_x_continuous(limits = c(0, 25))
+                     axis.line = element_line(size = 1), plot.title = element_text(size = 26, face = "bold"), 
+                     axis.ticks = element_line(color = "black", size = 1), axis.text = element_text(size = 26, color = "black"), 
+                     axis.title = element_text(size = 26, face = "bold"), legend.position = "none") + 
+  scale_y_continuous(limits = c(0, 25)) + scale_x_continuous(limits = c(0, 25))
 SSTmean_SSTmin_annotated_plot
 
 #SSTmean v. SSTmax plot
@@ -48,26 +51,33 @@ SSTmean_SSTmax_plot <- ggplot(data = BFs, aes(x = SST_Mean, y = SST_Max)) + geom
   geom_hline(yintercept = 15, size = 2, linetype = "dashed", color = "#666666") + 
   geom_vline(xintercept = 15, size = 2, linetype = "dashed", color = "#666666") + 
   geom_abline(intercept = 0, slope = 1, size = 2, color = "#999999") + 
-  annotate("text", x = 20, y = 7, label = "r = 0.215", size = 10)
+  annotate("text", x = 7, y = 20, label = "r = 0.363", size = 10) + 
+  annotate("text", x = 3, y = 25, label = "B", size = 15)
 SSTmean_SSTmax_annotated_plot <- SSTmean_SSTmax_plot + ggtitle("SST Mean BF v. SST Max BF") + labs(x = "SST Mean BF", y = "SST Max BF") + 
   theme_bw() + theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
-                     axis.line = element_line(size = 1), plot.title = element_text(size = 14, face = "bold"), 
-                     axis.ticks = element_line(color = "black", size = 1), axis.text = element_text(size = 30, color = "black"), 
-                     axis.title = element_text(size = 18, face = "bold")) + scale_y_continuous(limits = c(0, 25)) + scale_x_continuous(limits = c(0, 25))
+                     axis.line = element_line(size = 1), plot.title = element_text(size = 26, face = "bold"), 
+                     axis.ticks = element_line(color = "black", size = 1), axis.text = element_text(size = 26, color = "black"), 
+                     axis.title = element_text(size = 26, face = "bold"), legend.position = "none") + 
+  scale_y_continuous(limits = c(0, 25)) + scale_x_continuous(limits = c(0, 25))
 SSTmean_SSTmax_annotated_plot
 
 #SSTmin v. SSTmax plot
 SSTmin_SSTmax_plot <- ggplot(data = BFs, aes(x = SST_Min, y = SST_Max)) + geom_point(aes(size = 0.5)) + 
-  geom_hline(yintercept = 15, size = 1.5, linetype = "dashed", color = "#666666") + 
-  geom_vline(xintercept = 15, size = 1.5, linetype = "dashed", color = "#666666") + 
-  geom_abline(intercept = 0, slope = 1, size = 1.5, color = "#999999") + 
-  annotate("text", x = 20, y = 7, label = "r = 0.293", size = 4)
+  geom_hline(yintercept = 15, size = 2, linetype = "dashed", color = "#666666") + 
+  geom_vline(xintercept = 15, size = 2, linetype = "dashed", color = "#666666") + 
+  geom_abline(intercept = 0, slope = 1, size = 2, color = "#999999") + 
+  annotate("text", x = 7, y = 20, label = "r = 0.432", size = 10) + 
+  annotate("text", x = 3, y = 25, label = "C", size = 15)
 SSTmin_SSTmax_annotated_plot <- SSTmin_SSTmax_plot + ggtitle("SST Min BF v. SST Max BF") + labs(x = "SST Min BF", y = "SST Max BF") + 
   theme_bw() + theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
-                     axis.line = element_line(size = 1), plot.title = element_text(size = 14, face = "bold"), 
-                     axis.ticks = element_line(color = "black", size = 1), axis.text = element_text(size = 14, color = "black"), 
-                     axis.title = element_text(size = 18, face = "bold")) + scale_y_continuous(limits = c(0, 25)) + scale_x_continuous(limits = c(0, 25))
+                     axis.line = element_line(size = 1), plot.title = element_text(size = 26, face = "bold"), 
+                     axis.ticks = element_line(color = "black", size = 1), axis.text = element_text(size = 26, color = "black"), 
+                     axis.title = element_text(size = 26, face = "bold"), legend.position = "none") + 
+  scale_y_continuous(limits = c(0, 25)) + scale_x_continuous(limits = c(0, 25))
 SSTmin_SSTmax_annotated_plot
+
+BF_crrelation_all_plot <- grid.arrange(SSTmean_SSTmin_annotated_plot, SSTmean_SSTmax_annotated_plot, SSTmin_SSTmax_annotated_plot, ncol = 3)
+BF_correlation_all_plot
 
 ###############################################################################################################################################
 
@@ -75,40 +85,46 @@ SSTmin_SSTmax_annotated_plot
 
 #SSTmean v. XtX plot
 SSTmean_XtX_plot <- ggplot(data = BFs, aes(x = SST_Mean, y = M_XtX)) + geom_point(aes(size = 0.5)) + 
-  geom_hline(yintercept = 5.78, size = 2, color = "#666666", linetype = "dashed") + 
+  geom_hline(yintercept = 5.99, size = 2, color = "#666666", linetype = "dashed") + 
   geom_vline(xintercept = 15, size = 2, color = "#666666", linetype = "dashed") + 
+  annotate("rect", xmin = 15, xmax = Inf, ymin = 5.99, ymax = Inf, fill = "darkolivegreen", alpha = 0.4) +
   annotate("text", x = 3, y = 10, label = "A", size = 15)
 SSTmean_XtX_annotated_plot <- SSTmean_XtX_plot + labs(x = "SST Mean BF", y = "XtX") + 
   theme_bw() + theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
-                     axis.line = element_line(size = 1), plot.title = element_text(size = 14, face = "bold"), 
-                     axis.ticks = element_line(color = "black", size = 1), axis.text = element_text(size = 30, color = "black"), 
-                     axis.title = element_text(size = 18, face = "bold"), legend.justification = c(1, 0), legend.position = c(0.85, 0.1), 
-                     legend.text = element_text(size = 20), legend.title = element_text(size = 20)) + scale_y_continuous(limits = c(0, 10)) + scale_x_continuous(limits = c(0, 30)) + scale_color_manual(values = c("#CCCCCC", "#000000"), name = "Outlier", breaks = c("No", "Yes"), labels = c("No", "Yes")) + scale_size(guide = "none") + guides(color = guide_legend(override.aes = list(size = 4)))
+                     axis.line = element_line(size = 1), plot.title = element_text(size = 26, face = "bold"), 
+                     axis.ticks = element_line(color = "black", size = 1), axis.text = element_text(size = 26, color = "black"), 
+                     axis.title = element_text(size = 26, face = "bold"), legend.justification = c(1, 0), legend.position = c(0.85, 0.1), 
+                     legend.text = element_text(size = 20), legend.title = element_text(size = 20)) + 
+  scale_y_continuous(limits = c(0, 10)) + scale_x_continuous(limits = c(0, 30)) + 
+  scale_color_manual(values = c("#CCCCCC", "#000000"), name = "Outlier", breaks = c("No", "Yes"), labels = c("No", "Yes")) + 
+  scale_size(guide = "none") + guides(color = guide_legend(override.aes = list(size = 4)))
 SSTmean_XtX_annotated_plot
 
 #SSTmin v. XtX plot
-SSTmin_XtX_plot <- ggplot(data = BFs, aes(x = SST_Min, y = M_XtX)) + geom_point(aes(size = 0.5)) + 
-  geom_hline(yintercept = 5.78, size = 2, color = "#666666", linetype = "dashed") + 
+SSTmin_XtX_plot <- ggplot(data = BFs, aes(x = BFs$SST_Min, y = BFs$M_XtX)) + geom_point(aes(size = 0.5)) + 
+  geom_hline(yintercept = 5.99, size = 2, color = "#666666", linetype = "dashed") + 
   geom_vline(xintercept = 15, size = 2, color = "#666666", linetype = "dashed") + 
+  annotate("rect", xmin = 15, xmax = Inf, ymin = 5.99, ymax = Inf, fill = "darkolivegreen", alpha = 0.4) +
   annotate("text", x = 3, y = 10, label = "B", size = 15)
 SSTmin_XtX_annotated_plot <- SSTmin_XtX_plot + labs(x = "SST Min BF", y = "XtX") + 
   theme_bw() + theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
-                     axis.line = element_line(size = 1), plot.title = element_text(size = 14, face = "bold"), 
-                     axis.ticks = element_line(color = "black", size = 1), axis.text = element_text(size = 30, color = "black"), 
-                     axis.title = element_text(size = 18, face = "bold"), legend.justification = c(1, 0), legend.position = c(0.85, 0.1), 
+                     axis.line = element_line(size = 1), plot.title = element_text(size = 26, face = "bold"), 
+                     axis.ticks = element_line(color = "black", size = 1), axis.text = element_text(size = 26, color = "black"), 
+                     axis.title = element_text(size = 26, face = "bold"), legend.justification = c(1, 0), legend.position = c(0.85, 0.1), 
                      legend.text = element_text(size = 20), legend.title = element_text(size = 20)) + scale_y_continuous(limits = c(0, 10)) + scale_x_continuous(limits = c(0, 30)) + scale_color_manual(values = c("#CCCCCC", "#000000"), name  = "Outlier", breaks = c("No", "Yes"), labels = c("No", "Yes")) + scale_size(guide = "none") + guides(color = guide_legend(override.aes = list(size = 4)))
 SSTmin_XtX_annotated_plot
 
 #SSTmax v. XtX plot
 SSTmax_XtX_plot <- ggplot(data = BFs, aes(x = SST_Max, y = M_XtX)) + geom_point(aes(size = 0.5)) + 
-  geom_hline(yintercept = 5.78, size = 2, color = "#666666", linetype = "dashed") + 
+  geom_hline(yintercept = 5.99, size = 2, color = "#666666", linetype = "dashed") + 
   geom_vline(xintercept = 15, size = 2, color = "#666666", linetype = "dashed") + 
+  annotate("rect", xmin = 15, xmax = Inf, ymin = 5.99, ymax = Inf, fill = "darkolivegreen", alpha = 0.4) + 
   annotate("text", x = 3, y = 10, label = "C", size = 15)
 SSTmax_XtX_annotated_plot <- SSTmax_XtX_plot + labs(x = "SST Max BF", y = "XtX") + 
   theme_bw() + theme(panel.border = element_blank(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
-                     axis.line = element_line(size = 1), plot.title = element_text(size = 14, face = "bold"), 
-                     axis.ticks = element_line(color = "black", size = 1), axis.text = element_text(size = 30, color = "black"), 
-                     axis.title = element_text(size = 18, face = "bold"), legend.justification = c(1, 0), legend.position = c(0.85,0.1), 
+                     axis.line = element_line(size = 1), plot.title = element_text(size = 26, face = "bold"), 
+                     axis.ticks = element_line(color = "black", size = 1), axis.text = element_text(size = 26, color = "black"), 
+                     axis.title = element_text(size = 26, face = "bold"), legend.justification = c(1, 0), legend.position = c(0.85,0.1), 
                      legend.text = element_text(size = 20), legend.title = element_text(size = 20)) + scale_y_continuous(limits = c(0, 10)) + scale_x_continuous(limits = c(0, 30)) + scale_color_manual(values = c("#CCCCCC", "#000000"), name = "Outlier", breaks = c("No", "Yes"), labels = c("No", "Yes")) + scale_size(guide = "none") + guides(color = guide_legend(override.aes = list(size = 4)))
 SSTmax_XtX_annotated_plot
 
