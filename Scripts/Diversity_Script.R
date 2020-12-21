6e661bc6f4b2fb7bb87944b344b4122a9181e6d1
rclark848 <rclark848@gmail.com>
2020-12-21 11:02:19 -0500

    removed mac1 and Fis with adegenet sections

--- Scripts/Diversity_Script.R
+++ Scripts/Diversity_Script.R
@@ -17,7 +17,6 @@ remove(list = ls())
 library(adegenet)
 library(pegas)
 library(hierfstat)
-library(diveRsity)
 library(tidyverse)
 library(boot)
 
@@ -25,11 +24,7 @@ library(boot)
 SNPs_mac2 <- read.genetix("../../VCFs_and_PLINK/output.hicov2.snps.only.mac2_genetix.gtx") #convert genetix file to genind object for use in R (filtered SNPs for those with mac >/= 2) (maf >/= 0.05))
 allele_counts <- read_table2("Data/clownfish_mac2_geno.txt", col_names = TRUE) #read in allele count input data for BayPass
 locnames <- read_table2("Data/Loc_Names_mac2.txt", col_names = TRUE) #read in contig & bp for loci passing mac2 filter
-dim(locnames) #4212 x 1
-SNPs_mac1 <- read.genetix("../../VCFs_and_PLINK/mac1_genetix.gtx")
-allele_counts_mac1 <- read_table2("Data/clownfish_mac1_geno.txt", col_names = TRUE)
-locnames_mac1 <- read_table2("Data/Loc_names_mac1.txt", col_names = FALSE)
-  colnames(locnames_mac1) <- c("Contig_bp")
+  dim(locnames) #4212 x 1
 
 ################################################################################################################################################
 
@@ -57,100 +52,49 @@ I_Hs <- apply(allele_counts[,5:6], 1, heterozygosity)
 #calculate Fis per locus in each pop
 J_header <- c("J_Ho", "J_Hs")
 J_diversity <- data.frame(J_Ho, J_Hs) #combine het metrics into same data frame
-colnames(J_diversity) <- J_header #set header
+  colnames(J_diversity) <- J_header #set header
 J_diversity$J_Fis <- 1 - (J_diversity$J_Ho/J_diversity$J_Hs) #calculate Fis per locus
 
 P_header <- c("P_Ho", "P_Hs")
 P_diversity <- data.frame(P_Ho, P_Hs)
-colnames(P_diversity) <- P_header
+  colnames(P_diversity) <- P_header
 P_diversity$P_Fis <- 1 - (P_diversity$P_Ho/P_diversity$P_Hs)
 
 I_header <- c("I_Ho", "I_Hs")
 I_diversity <- data.frame(I_Ho, I_Hs)
-colnames(I_diversity) <- I_header
+  colnames(I_diversity) <- I_header
 I_diversity$I_Fis <- 1 - (I_diversity$I_Ho/I_diversity$I_Hs)
 
 #combine to one data frame and write out
 diversity_tot <- data.frame(J_diversity, P_diversity, I_diversity)
 diversity_tot$snp_id <- snps
 diversity_tot <- merge(diversity_tot, locnames, by="snp_id")
-diversity_tot <- diversity_tot[2:11]
+  diversity_tot <- diversity_tot[2:11] #remove snp_id indexing
 write.csv(diversity_tot, file = "Data/mac2_diversity.csv")
-
-################################################################################################################################################
-
-######## Compute diversity statistics w/only SNPs w/mac >/= 1 ########
-
-#format data
-snps_mac1 <- c(1:5718) #create column for indexing
-locnames_mac1$snp_id <- snps_mac1
-allele_counts_mac1$snp_id <- snps_mac1
-
-allele_counts_mac1 <- merge(allele_counts_mac1, locnames_mac1, by = "snp_id") #merge allele_counts and locnames so know which loci corresponds with what allele frequencies
-allele_counts_mac1 <- allele_counts_mac1[,2:8] #remove snp_id index
-
-#calculate Ho per locus in each pop
-sum_stats_mac1 <- basic.stats(SNPs_mac1) #hierfstat function --> gives Ho, Hs, Fis on locus basis/pop as well as some pop-wide summary stats (Hs from Nei & Chesser 1983)
-J_Ho_mac1 <- data.frame(sum_stats_mac1$Ho[,1]) #pull out Ho for J pop
-I_Ho_mac1 <- data.frame(sum_stats_mac1$Ho[,2])
-P_Ho_mac1 <- data.frame(sum_stats_mac1$Ho[,3])
-
-#calculate Hs (~He) per locus in each pop
-J_Hs_mac1 <- apply(allele_counts_mac1[,1:2], 1, heterozygosity) #pegas heterozygosity function to calculate Nei's average gene diversity w/correction for small sample sizes
-P_Hs_mac1 <- apply(allele_counts_mac1[,3:4], 1, heterozygosity)
-I_Hs_mac1 <- apply(allele_counts_mac1[,5:6], 1, heterozygosity)
-
-#calculate Fis per locus in each pop
-J_header <- c("J_Ho", "J_Hs")
-J_diversity_mac1 <- data.frame(J_Ho_mac1, J_Hs_mac1) #combine het metrics into same data frame
-colnames(J_diversity_mac1) <- J_header #set header
-J_diversity_mac1$J_Fis <- 1 - (J_diversity_mac1$J_Ho/J_diversity_mac1$J_Hs) #calculate Fis per locus
-
-P_header <- c("P_Ho", "P_Hs")
-P_diversity_mac1 <- data.frame(P_Ho_mac1, P_Hs_mac1)
-colnames(P_diversity_mac1) <- P_header
-P_diversity_mac1$P_Fis <- 1 - (P_diversity_mac1$P_Ho/P_diversity_mac1$P_Hs)
-
-I_header <- c("I_Ho", "I_Hs")
-I_diversity_mac1 <- data.frame(I_Ho_mac1, I_Hs_mac1)
-colnames(I_diversity_mac1) <- I_header
-I_diversity_mac1$I_Fis <- 1 - (I_diversity_mac1$I_Ho/I_diversity_mac1$I_Hs)
-
-#combine to one data frame and write out
-diversity_tot_mac1 <- data.frame(J_diversity_mac1, P_diversity_mac1, I_diversity_mac1)
-diversity_tot_mac1$snp_id <- snps_mac1
-diversity_tot_mac1 <- merge(diversity_tot_mac1, locnames_mac1, by="snp_id")
-diversity_tot_mac1 <- diversity_tot_mac1[2:11]
-write.csv(diversity_tot_mac1, file = "Data/mac1_diversity.csv")
 
 ###############################################################################################################################################
 
 ######## Bootstrapping for Fis ########
 
+#write function to calculate mean
 samp_mean <- function(x, i) {
   mean(x[i])
 }
 
 #bootstrap for J pop Fis
-J_Fis <- data.frame(diversity_tot_mac1$J_Fis)
+J_Fis <- data.frame(diversity_tot$J_Fis)
   colnames(J_Fis) <- c("Fis")
 J_Fis <- subset(J_Fis, !is.na(J_Fis$Fis)) #remove NAs
-J_Fis_addback <- data.frame(matrix(0, nrow = 2221, ncol = 1)) #add back NAs as 0s
-  colnames(J_Fis_addback) <- c("Fis")
-J_Fis <- rbind(J_Fis, J_Fis_addback) #rbind dataframes to get full list of Fis values
-J_Fismean <- mean(J_Fis$Fis) #calculate mean (-0.033956)
+J_Fismean <- mean(J_Fis$Fis) #calculate mean (-0.06498  )
 
 boot_J_Fis <- boot(data = J_Fis$Fis, statistic = samp_mean, R = 1000) #1000 permutations of pi
 J_95ci_Fis <- boot.ci(boot_J_Fis, conf = 0.95, type = "norm") #get 95% CI for pi
 J_95ci_Fis_normal <- J_95ci_Fis$normal #pull out normal distribution  2.5 & 97.5 percentiles for pi
 
 #bootstrap for P pop Fis
-P_Fis <- data.frame(diversity_tot_mac1$P_Fis)
+P_Fis <- data.frame(diversity_tot$P_Fis)
   colnames(P_Fis) <- c("Fis")
 P_Fis <- subset(P_Fis, !is.na(P_Fis$Fis)) #remove NAs
-P_Fis_addback <- data.frame(matrix(0, nrow = 1452, ncol = 1)) #add back NAs as 0s
-  colnames(P_Fis_addback) <- c("Fis")
-P_Fis <- rbind(P_Fis, P_Fis_addback) #rbind dataframes to get full list of Fis values
 P_Fismean <- mean(P_Fis$Fis) #calculate mean (0.013314)
 
 boot_P_Fis <- boot(data = P_Fis$Fis, statistic = samp_mean, R = 1000) #1000 permutations of pi
@@ -158,12 +102,9 @@ P_95ci_Fis <- boot.ci(boot_P_Fis, conf = 0.95, type = "norm") #get 95% CI for pi
 P_95ci_Fis_normal <- P_95ci_Fis$normal #pull out normal distribution  2.5 & 97.5 percentiles for pi
 
 #bootstrap for N pop Fis
-N_Fis <- data.frame(diversity_tot_mac1$I_Fis)
+N_Fis <- data.frame(diversity_tot$I_Fis)
   colnames(N_Fis) <- c("Fis")
 N_Fis <- subset(N_Fis, !is.na(N_Fis$Fis)) #remove NAs
-N_Fis_addback <- data.frame(matrix(0, nrow = 2267, ncol = 1)) #add back NAs as 0s
-  colnames(N_Fis_addback) <- c("Fis")
-N_Fis <- rbind(N_Fis, N_Fis_addback) #rbind dataframes to get full list of Fis values
 N_Fismean <- mean(N_Fis$Fis) #calculate mean (-0.002233)
 
 boot_N_Fis <- boot(data = N_Fis$Fis, statistic = samp_mean, R = 1000) #1000 permutations of pi
@@ -176,42 +117,11 @@ rownames(Fis_mean) <- c("Japan", "Philippines", "Indonesia")
 
 Fis_norm_ci <- rbind(J_95ci_Fis_normal, P_95ci_Fis_normal, N_95ci_Fis_normal) #combine df w/ci info for each pop into one dataframe
 Fis_sum <- cbind(Fis_mean, Fis_norm_ci)
-colnames(Fis_sum) <- c("mean", "ci", "2.5_per", "97.5_per")
+  colnames(Fis_sum) <- c("mean", "ci", "2.5_per", "97.5_per")
 Fis_sum$Pop <- c("Japan", "Philippines", "Indonesia")
 
 Fis_sum$diff_lower <- Fis_sum$mean - Fis_sum$`2.5_per` #calculate diff btwn sample mean and 2.5 percentile for CI visualization
 Fis_sum$diff_upper <- Fis_sum$`97.5_per` - Fis_sum$mean # calculate diff btwn sample mean and 97.5 percentile for CI visualization
 
 #write out data
-write.csv(Fis_sum, "Data/Fis_cis_mac1.csv")
-
-###############################################################################################################################################
-
-######## (Optional) inbreeding measure (Fis) with adegenet (SNPs w/mac>2) ########
-
-#set population names
-popNames(SNPs_mac1) <- c("Japan", "Indonesia", "Philippines")
-popNames(SNPs_mac1) #check to make sure popnames are right
-
-#subset dataframe to each population
-J_inds <- SNPs_mac1[1:8, ]
-J_sum <- summary(J_inds) #get summary to make sure subsetting correctly
-I_inds <- SNPs_mac1[9:15, ]
-I_inds <- SNPs_mac1[16:25, ]
-
-#inbreeding measure
-J_inbred <- inbreeding(J_inds) #creates likelihood distribution for Fis of each locus
-J_inbred_mean_mac1 <- sapply(J_inbred, mean) #mean likelihood
-hist(J_inbred_mean_mac1) #plot
-
-P_inbred <- inbreeding(P_inds)
-P_inbred_mean <- sapply(P_inbred, mean)
-hist(P_inbred_mean)
-
-I_inbred <- inbreeding(I_inds)
-I_inbred_mean <- sapply(I_inbred, mean)
-hist(I_inbred_mean)
-
-#Genepop_mac2 <- readGenepop(infile = "Data/output.hicov2.snps.only.mac2_genpop.gen", gp = 3, bootstrap = FALSE)
-#basicstats <- divBasic(infile = "Data/output.hicov2.snps.only.mac2_genpop.gen", outfile = NULL, gp = 3, bootstraps = 0)
-#allele <- Genepop_mac2$all_alleles
+write.csv(Fis_sum, "Data/Fis_cis_mac2.csv")
