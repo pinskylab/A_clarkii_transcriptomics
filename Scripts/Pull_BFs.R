59afdbcc4373d04cae38934adc8a7f5f14d4518e
rclark848 <rclark848@gmail.com>
2021-01-05 14:35:38 -0500

    Removed mac1 stuff; changed level of BF cut-off

--- Scripts/Pull_BFs.R
+++ Scripts/Pull_BFs.R
@@ -15,11 +15,11 @@ getwd()
 library(readr)
 
 #read in BFs
-BFs_ssmean <- read_table2("../../BayPass_Output/mac1_SNPs/sim_stuff/summary_betai/mac1sim10_ssmean_summary_betai.txt", col_names = TRUE) #use read_table2 because uneven amount of white spaces between columns
-BFs_sstmean <- read_table2("../../BayPass_Output/mac1_SNPs/sim_stuff/summary_betai/mac1sim10_sstmean_summary_betai.txt", col_names = TRUE) #use read_table2 because uneven amount of white spaces between columns
-BFs_sstmin <- read_table2("../../BayPass_Output/mac1_SNPs/sim_stuff/summary_betai/mac1sim10_sstmin_summary_betai.txt", col_names = TRUE) #use read_table2 because uneven amount of white spaces between columns
-BFs_sstmax <- read_table2("../../BayPass_Output/mac1_SNPs/sim_stuff/summary_betai/mac1sim10_sstmax_summary_betai.txt", col_names = TRUE) #use read_table2 because uneven amount of white spaces between columns
-BFs_lat <- read_table2("../../BayPass_Output/mac1_SNPs/sim_stuff/summary_betai/mac1sim10_lat_summary_betai.txt", col_names = TRUE) #use read_table2 because uneven amount of white spaces between columns
+BFs_ssmean <- read_table2("../../BayPass_Output/mac2_SNPs/aux_output/mac2aux1_ssmean_summary_betai.txt", col_names = TRUE) #use read_table2 because uneven amount of white spaces between columns
+BFs_sstmean <- read_table2("../../BayPass_Output/mac2_SNPs/aux_output/mac2aux1_sstmean_summary_betai.txt", col_names = TRUE) #use read_table2 because uneven amount of white spaces between columns
+BFs_sstmin <- read_table2("../../BayPass_Output/mac2_SNPs/aux_output/mac2aux1_sstmin_summary_betai.txt", col_names = TRUE) #use read_table2 because uneven amount of white spaces between columns
+BFs_sstmax <- read_table2("../../BayPass_Output/mac2_SNPs/aux_output/mac2aux1_sstmax_summary_betai.txt", col_names = TRUE) #use read_table2 because uneven amount of white spaces between columns
+BFs_lat <- read_table2("../../BayPass_Output/mac2_SNPs/aux_output/mac2aux1_lat_summary_betai.txt", col_names = TRUE) #use read_table2 because uneven amount of white spaces between columns
 
 ################################################################################################################################################
 
@@ -38,51 +38,51 @@ markers <- c(BFs_ssmean[1:5718, 'MRK'])
 
 #Create data frame with markers & BFs for each covariable as rows
 BFs_ALL <- data.frame(markers, BFs_SSSmean, BFs_SSTmean, BFs_SSTmin, BFs_SSTmax, BFs_lat)
-names(BFs_ALL) <- c("snp_id", "SSS_Mean", "SST_Mean", "SST_Min", "SST_Max", "Lat")
+  names(BFs_ALL) <- c("snp_id", "SSS_Mean", "SST_Mean", "SST_Min", "SST_Max", "Lat")
 
 ######## Pull out putative candidate loci by env variable ########
 
-#pull loci # with BF > 15 for each environmental covariate
+#pull loci # with BF > 20 for each environmental covariate
 #pull for col 2: SSS Mean
-BFs_greater_than_15_SSSmean <- c(which(BFs_ALL[, 2] > 15))
+BFs_greater_than20_SSSmean <- c(which(BFs_ALL[, 2] > 20))
 
 #Pull for col 3: SST Mean
-BFs_greater_than15_SSTmean <- c(which(BFs_ALL[, 3] > 15))
+BFs_greater_than20_SSTmean <- c(which(BFs_ALL[, 3] > 20))
 
 #Pull for col 4: SST Min
-BFs_greater_than15_SSTmin <- c(which(BFs_ALL[, 4] > 15))
+BFs_greater_than20_SSTmin <- c(which(BFs_ALL[, 4] > 20))
 
 #Pull for col 5: SST Max
-BFs_greater_than15_SSTmax <- c(which(BFs_ALL[, 5] > 15))
+BFs_greater_than20_SSTmax <- c(which(BFs_ALL[, 5] > 20))
 
 #Pull for col 6: Latitude
-BFs_greater_than15_Latitude <- c(which(BFs_ALL[, 6] > 15))
+BFs_greater_than20_Latitude <- c(which(BFs_ALL[, 6] > 20))
 
 ######## Pull candidate loci names ########
 
 #create vector of potential candidate loci
-potential_loci_list <- BFs_greater_than_15_SSSmean
+potential_loci_list <- BFs_greater_than20_SSSmean
 
 #add candidate loci flagged from other covariates
-potential_loci_list <- append(potential_loci_list, BFs_greater_than15_SSTmean)
-potential_loci_list <- append(potential_loci_list, BFs_greater_than15_SSTmin)
-potential_loci_list <- append(potential_loci_list, BFs_greater_than15_SSTmax)
-potential_loci_list <- append(potential_loci_list, BFs_greater_than15_Latitude)
+potential_loci_list <- append(potential_loci_list, BFs_greater_than20_SSTmean)
+potential_loci_list <- append(potential_loci_list, BFs_greater_than20_SSTmin)
+potential_loci_list <- append(potential_loci_list, BFs_greater_than20_SSTmax)
+potential_loci_list <- append(potential_loci_list, BFs_greater_than20_Latitude)
 
 ######## Grab ID names of the contigs containing candidate SNPs ########
 
 #read in loc names
 #locnames <- read_table2("Data/Loc_Names_All.txt", col_names = TRUE)
-locnames <- read_table2("Data/Loc_Names_mac1.txt", col_names = FALSE)
-dim(locnames) #5718
+locnames <- read_table2("Data/Loc_Names_mac2.txt", col_names = FALSE)
+  dim(locnames) #4213
 
-#snps <- c(1:5729) #create column to match "marker" column pulled earlier from aux1_summary_betai.txt
-snps <- c(1:5718)
+#snps <- c(1:4213) #create column to match "marker" column pulled earlier from aux1_summary_betai.txt
+snps <- c(1:4213)
 locnames$snp_id <- snps
 
 #filter potential_loci_list so only one instance of each potential can loci
 can_loci <- unique(sort(potential_loci_list)) #keep only unique loci (ignore repeats)
-can_names <- locnames[c(can_loci),] #create dataframe of can loci that have been flagged with BF > 15
+can_names <- locnames[c(can_loci),] #create dataframe of can loci that have been flagged with BF > 20
 can_BFs <- BFs_ALL[c(can_loci),] #create dataframe of BFs for can loci for each env variable
 
 #merge dataframes
@@ -90,5 +90,4 @@ can_all <- merge(can_names, can_BFs, by = "snp_id")
 all <- merge(locnames, BFs_ALL, by = "snp_id")
 
 #write out
-#write.table(can_loci, file = "Data/mac2aux1_original_all_candidate_loci.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
-write.table(all, file = "Data/mac1sim10_all_BFs.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
+write.table(can_loci, file = "Data/mac2aux1_original_all_candidate_loci.txt", sep = "\t", row.names = FALSE, col.names = TRUE)
