bd8dccf052e73c0084a60259ac4ea63e08094335
rclark848 <rclark848@gmail.com>
2021-02-04 14:41:00 -0500

    annotated and added notes

--- Scripts/TD_v_pi.R
+++ Scripts/TD_v_pi.R
@@ -1,7 +1,20 @@
-######## Visualize data ########
-#designed to be run separately from earlier sections
+################################ Script for Creating Tajima's D v. Pi Plots  ########################################################
+
+#Uses Tajima's D estimates from VCFtools --> only synonymous sites incorporated into calculations (ID'd from mapping to A. frenatus)
+#Uses pi estimates (across contig) from VCFtools --> only synonymous sites as well
+
+#################################################################################################################################################
+
+######## Set-up ########
+
+#set working directory
+setwd("C:/Users/Rene/Dropbox/Pinsky_Lab/Transcriptome_Proj/R_scripts/A_clarkii_transcriptomics/")
+getwd()
 
 remove(list = ls())
+
+#load libraries
+library(tidyverse)
 
 #read in data
 TD_only_all <- read.csv("Data/SYN_mac1_TajimasD_combined_full.csv", header = TRUE, row.names = 1)
@@ -11,6 +24,10 @@ pi_only_all <- read.csv("Data/pi_combined_site_full_mac1_SYN_aggregate.csv", hea
 
 data_all <- cbind(TD_only_all, pi_only_all$pi_avg)
   colnames(data_all) <- c("CHROM", "BIN_START", "N_SNPS", "TajimaD", "uniqseq", "Pop", "NUM", "pi")
+
+#################################################################################################################################################
+  
+######## Format data ########
 
 #add outlier status
 outliers <- data_all[data_all$uniqseq %in% outlierseq$CONTIG, ] #df with only outliers
@@ -25,14 +42,21 @@ data_all$Status <- c(rep("All", times = 1992))
 data_all <- rbind(outliers, nonoutliers, data_all)
 data_all$Status <- factor(data_all$Status, levels = c("All", "Not_Outlier", "Outlier"))
 
+#subset to sampling sites
 all_only <- subset(data_all, Pop == "All" & Status != "All")
 J_only <- subset(data_all, Pop == "Japan" & Status != "All")
 N_only <- subset(data_all, Pop == "Indonesia" & Status != "All")
 P_only <- subset(data_all, Pop == "Philippines" & Status != "All")
 
+#subset to outliers only
 outliers_only <- subset(data_all, Status == "Outlier")
 
-#all seqs by pop
+#################################################################################################################################################
+
+######## Visualize data ########
+#Scatterplots of Tajima's D v. pi
+
+#TD v pi for pooled individuals
 all_plot <- ggplot(data = all_only, aes(x = pi, y = TajimaD, color = Status)) + 
   geom_point(size = 8) + geom_hline(yintercept = 0, color = "black", size = 3)
 all_plot_annotated <- all_plot + theme_bw() + 
@@ -44,6 +68,7 @@ all_plot_annotated <- all_plot + theme_bw() +
         legend.text = element_text(size = 32), legend.title = element_text(size = 32))
 all_plot_annotated
 
+#TD v pi for Japan
 J_plot <- ggplot(data = J_only, aes(x = pi, y = TajimaD, color = Status)) + 
   geom_point(size = 8) + geom_hline(yintercept = 0, color = "black", size = 3)
 J_plot_annotated <- J_plot + theme_bw() + 
@@ -55,6 +80,7 @@ J_plot_annotated <- J_plot + theme_bw() +
         legend.text = element_text(size = 32), legend.title = element_text(size = 32))
 J_plot_annotated
 
+#TD v pi for Philippines
 P_plot <- ggplot(data = P_only, aes(x = pi, y = TajimaD, color = Status)) + 
   geom_point(size = 8) + geom_hline(yintercept = 0, color = "black", size = 3)
 P_plot_annotated <- P_plot + theme_bw() + 
@@ -66,6 +92,7 @@ P_plot_annotated <- P_plot + theme_bw() +
         legend.text = element_text(size = 32), legend.title = element_text(size = 32))
 P_plot_annotated
 
+#TD v pi for Indonesia
 N_plot <- ggplot(data = N_only, aes(x = pi, y = TajimaD, color = Status)) + 
   geom_point(size = 8) + geom_hline(yintercept = 0, color = "black", size = 3)
 N_plot_annotated <- N_plot + theme_bw() + 
@@ -77,6 +104,7 @@ N_plot_annotated <- N_plot + theme_bw() +
         legend.text = element_text(size = 32), legend.title = element_text(size = 32))
 N_plot_annotated
 
+#TD v pi with only outliers
 outliers_plot <- ggplot(data = outliers_only, aes(x = pi, y = TajimaD, color = Pop)) + 
   geom_point(size = 8) + geom_hline(yintercept = 0, color = "black", size = 3)
 outliers_plot_annotated <- outliers_plot + theme_bw() + 
