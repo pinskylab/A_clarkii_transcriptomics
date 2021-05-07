002e5e130741dcb95a03a9d9454dcf577132893c
rclark848 <rclark848@gmail.com>
2021-05-07 10:36:44 -0400

    cleaned up script and added post-processing info

--- Scripts/Bootstrap_forSFS.R
+++ Scripts/Bootstrap_forSFS.R
@@ -47,54 +47,11 @@ num <- c(1:5662)
 df <- as.data.frame(cbind(chrom, num))
 df$Contig <- gsub(" ", "", paste(df$chrom, df$num))
 
-#write out each df
-all91 <- cbind(df$Contig, all91)
-all91 <- all91[,-2]
-colnames(all91)[1] <- "#CHROM"
-write.table(all91, file = "Data/bootstrapped_vcfs_forSFS/mac1_nooutliers_reordered_boot91.txt", 
+#write out each df (repeat 10x, for 1-100 df)
+all1 <- cbind(df$Contig, all1)
+all1 <- all1[,-2]
+colnames(all1)[1] <- "#CHROM"
+write.table(all1, file = "Data/bootstrapped_vcfs_forSFS/mac1_nooutliers_reordered_boot1.txt", 
             sep = "\t",row.names = FALSE, col.names = TRUE, quote = FALSE)
-all92 <- cbind(df$Contig, all92)
-all92 <- all92[,-2]
-colnames(all92)[1] <- "#CHROM"
-write.table(all92, file = "Data/bootstrapped_vcfs_forSFS/mac1_nooutliers_reordered_boot92.txt", 
-            sep = "\t",row.names = FALSE, col.names = TRUE, quote = FALSE)
-all93 <- cbind(df$Contig, all93)
-all93 <- all93[,-2]
-colnames(all93)[1] <- "#CHROM"
-write.table(all93, file = "Data/bootstrapped_vcfs_forSFS/mac1_nooutliers_reordered_boot93.txt", 
-            sep = "\t",row.names = FALSE, col.names = TRUE, quote = FALSE)
-all94 <- cbind(df$Contig, all94)
-all94 <- all94[,-2]
-colnames(all94)[1] <- "#CHROM"
-write.table(all94, file = "Data/bootstrapped_vcfs_forSFS/mac1_nooutliers_reordered_boot94.txt", 
-            sep = "\t",row.names = FALSE, col.names = TRUE, quote = FALSE)
-all95 <- cbind(df$Contig, all95)
-all95 <- all95[,-2]
-colnames(all95)[1] <- "#CHROM"
-write.table(all95, file = "Data/bootstrapped_vcfs_forSFS/mac1_nooutliers_reordered_boot95.txt", 
-            sep = "\t",row.names = FALSE, col.names = TRUE, quote = FALSE)
-all96 <- cbind(df$Contig, all96)
-all96 <- all96[,-2]
-colnames(all96)[1] <- "#CHROM"
-write.table(all96, file = "Data/bootstrapped_vcfs_forSFS/mac1_nooutliers_reordered_boot96.txt", 
-            sep = "\t",row.names = FALSE, col.names = TRUE, quote = FALSE)
-all97 <- cbind(df$Contig, all97)
-all97 <- all97[,-2]
-colnames(all97)[1] <- "#CHROM"
-write.table(all97, file = "Data/bootstrapped_vcfs_forSFS/mac1_nooutliers_reordered_boot97.txt", 
-            sep = "\t",row.names = FALSE, col.names = TRUE, quote = FALSE)
-all98 <- cbind(df$Contig, all98)
-all98 <- all98[,-2]
-colnames(all98)[1] <- "#CHROM"
-write.table(all98, file = "Data/bootstrapped_vcfs_forSFS/mac1_nooutliers_reordered_boot98.txt", 
-            sep = "\t",row.names = FALSE, col.names = TRUE, quote = FALSE)
-all99 <- cbind(df$Contig, all99)
-all99 <- all99[,-2]
-colnames(all99)[1] <- "#CHROM"
-write.table(all99, file = "Data/bootstrapped_vcfs_forSFS/mac1_nooutliers_reordered_boot99.txt", 
-            sep = "\t",row.names = FALSE, col.names = TRUE, quote = FALSE)
-all100 <- cbind(df$Contig, all100)
-all100 <- all100[,-2]
-colnames(all100)[1] <- "#CHROM"
-write.table(all100, file = "Data/bootstrapped_vcfs_forSFS/mac1_nooutliers_reordered_boot100.txt", 
-            sep = "\t",row.names = FALSE, col.names = TRUE, quote = FALSE)
+
+#after write out all 100, add in VCF header info (in txt editor) and save as ".vcf"
